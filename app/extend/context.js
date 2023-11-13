2cedd4c301972f5b1ba5500e411abbd31745d787
一剑 <answord@163.com>
2023-11-13 16:07:43 +0800

    升级到1.1.2，context.js 增加 success 和 fail 响应方法

--- app/extend/context.js
+++ app/extend/context.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const RES_CODE_MSG = {
+  200: '成功',
+  400: '请求参数错误',
+  500: '服务器内部错误',
+};
+
 module.exports = {
+
+  /**
+   * 处理成功请求后的响应
+   */
+  success({ code = 200, msg, data }) {
+    this.status = 200;
+    this.body = {
+      code,
+      msg: msg || RES_CODE_MSG[code] || '',
+      data,
+    };
+  },
+
+  /**
+   * 处理失败请求后的响应
+   */
+  fail({ code = 500, msg, data }) {
+    this.status = 200;
+    this.body = {
+      code,
+      msg: msg || RES_CODE_MSG[code] || '',
+      data,
+    };
+  },
 
 };
